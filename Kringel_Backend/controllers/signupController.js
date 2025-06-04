ba6f16dd54b875ed0bf7c320e428259148f97680
lisettereins <lreins@tlu.ee>
2025-06-04 20:50:25 +0300

    Panin reacti püsti

--- Kringel_Backend/controllers/signupController.js
+++ Kringel_Backend/controllers/signupController.js
@@ -8,7 +8,7 @@ const signupPage =(req, res)=>{
 const signup = (req, res)=>{
     let notice = "Ootan andmeid.";
     console.log(req.body);
-    if(!req.body.firstNameInput || !req.body.lastNameInput || !req.body.birthDateInput || !req.body.genderInput || !req.body.emailInput || req.body.passwordInput.length < 8 || req.body.passwordInput !== req.body.confirmPasswordInput){
+    if(!req.body.emailInput || req.body.passwordInput.length < 8 || req.body.passwordInput !== req.body.confirmPasswordInput){
         console.log("Andmeid on puudu, või paroolid ei kattu!");
         notice ="Andmeid on puudu, parool liiga lühike, või paroolid ei kattu.";
         res.render("signup", {notice: notice});
@@ -29,8 +29,8 @@ const signup = (req, res)=>{
                         res.render("signup", {notice: notice});
                     }
                     else{
-                        let sqlReq = "INSERT INTO users (first_name, last_name, birth_date, gender, email, password) VALUES(?,?,?,?,?,?)";
-                        conn.execute(sqlReq, [req.body.firstNameInput, req.body.lastNameInput, req.body.birthDateInput, req.body.genderInput, req.body.emailInput, pwdHash], (err, result)=>{
+                        let sqlReq = "INSERT INTO users (email, password) VALUES(?,?)";
+                        conn.execute(sqlReq, [req.body.emailInput, pwdHash], (err, result)=>{
                             if(err){
                                 notice="Tehniline viga andmebaasi kirjutamisel, kasutajat ei loodud.";
                                 res.render("signup", {notice: notice});

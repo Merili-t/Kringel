e17ef7d2bc80846767349b1dfabd2757ae9b53af
Age Toomet <agett17@tlu.ee>
2025-06-10 13:59:26 +0300

    style fixes

--- main-frontend/script/tutorialHome.js
+++ main-frontend/script/tutorialHome.js
@@ -1,7 +1,7 @@
-document.querySelectorAll('.accordion-header').forEach(header => {
-    header.addEventListener('click', () => {
-      const item = header.parentElement;
-      item.classList.toggle('active');
-    });
-  });
+// document.querySelectorAll('.accordion-header').forEach(header => {
+//     header.addEventListener('click', () => {
+//       const item = header.parentElement;
+//       item.classList.toggle('active');
+//     });
+//   });
   

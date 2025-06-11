04e51fb5dd45e0e3d6507ed5784e55a806d434a1
Sven Laht <Sven.Laht@gmail.com>
2025-06-11 14:51:56 +0300

    Implement routes for test upload
    - Added routes to create and get tests.

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,7 +1,8 @@
 import e from 'express';
-import { getQuestion, postQuestion } from './controllers/questionController.js';
-import { getTests, postTests } from './controllers/testController.js';
-import { login, register, logout } from './controllers/authController.js';
+import * as test from './controllers/testController.js';
+import * as auth from './controllers/authController.js';
+import * as block from './controllers/blockController.js';
+import * as question from './controllers/questionController.js';
 import checkToken from './middleware/authMiddleware.js';
 import * as access from './middleware/accessControllMiddleware.js';
 
@@ -11,14 +12,24 @@ router.get('/', (req, res) => {
   res.json({ message: 'Welcome to Kringel API' });
 });
 
-router.post('/auth/login', checkToken, login);
-router.post('/auth/register', checkToken, register);
-router.get('/auth/logout', checkToken, access.level1, logout);
+// auth routes
+router.post('/auth/login', checkToken, auth.login);
+router.post('/auth/register', checkToken, auth.register);
+router.get('/auth/logout', checkToken, access.level1, auth.logout);
 
-router.get('/tests', checkToken, access.level2, getTests);
-router.post('/test/upload', checkToken, access.level2, postTests);
+// test routes
+router.post('/test/upload', checkToken, access.level2, test.upload);
+router.get('/test/tests', checkToken, access.level2, test.getTests);
+router.get('/test/:id', checkToken, access.level1, test.getByTestId);
 
-router.get('/questions', checkToken, access.level2, getQuestion);
-router.post('/question/upload', checkToken, access.level2, postQuestion);
+// block routes
+router.post('/block/upload', checkToken, access.level2, block.upload);
+router.get('/block/test/:testId', checkToken, access.level1, block.getByTestId);
+router.get('/block/:id', checkToken, access.level1, block.getByBlockId);
+
+// question routes
+router.post('/question/upload', checkToken, access.level2, question.upload);
+router.get('/question/block/:blockId', checkToken, access.level1, question.getByBlockId);
+router.get('/question/:id', checkToken, access.level1, question.getByQuestionId);
 
 export default router;
